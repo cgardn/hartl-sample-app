068ed2a203aa0a1b60e50d227a52c3c26892c2c1
cgardn <christopher.gardn@gmail.com>
2020-01-09 14:20:41 -0500

    add user name, email, password validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,74 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobarasdf", password_confirmation: "foobarasdf")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should be max 50 chars long" do
+    @user.name = 'a'*51;
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be max 255 chars long" do
+    @user.email = 'a'*244 + '@example.com';
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be downcased before saving" do
+    mixed_case_email = "aSdF@tEsT.cOm"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should not be blank" do
+    @user.password = @user.password_confirmation = " "*9
+    assert_not @user.valid?
+  end
+
+  test "password should be longer than 8 characters" do
+    @user.password = @user.password_confirmation = "a"*8
+    assert_not @user.valid?
+  end
+
 end
